264c4a21027654392ed13f47e651bd9faa7466cf
Scott Garvin <sgarvin842@outlook.com>
2024-05-05 16:11:55 +0000

    Added body parser for post requests. Also added initial scaffolding for /api/users, /api/users/:_id/exercisies, and /api/users/:id/logs endpoints

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
+
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => {
+  console.log(req.body.username);
+  res.send({ test: "test" });
+});
 
+app.post('/api/users/:_id/exercises', (req, res) => {
+  console.log(req.body.id);
+  console.log(req.body.description);
+  console.log(req.body.duration);
+  console.log(req.body.date);
+  res.send({ test: "test" });
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  console.log("return description, duration, date of all workouts");
+});
 
 
 
